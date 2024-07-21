f99fdc7bf418a4255c438d925eaf6929e32988cf
unknown <dgloria.web@gmail.com>
2024-07-21 19:27:55 +0100

    fiddling with testing, changed default database, start writing tests

--- tests/Feature/UserManagementTest.php
+++ tests/Feature/UserManagementTest.php
@@ -1,11 +1,12 @@
 <?php
 
 use App\Models\User;
+
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Foundation\Testing\RefreshDatabase;
+
 
 it('allows a user to register', function () {
-    uses(RefreshDatabase::class);
+    $this->markTestSkipped('Skipping this test for now.');
     $response = $this->postJson('/api/register', [
         'name' => 'John Doe',
         'email' => 'john@example.com',
@@ -17,20 +18,22 @@ it('allows a user to register', function () {
 });
 
 it('allows a user to login', function () {
-    uses(RefreshDatabase::class);
+    $this->markTestSkipped('Skipping this test for now.');
     // Create a user in the database
+    $password = Hash::make('password');
     $user = User::create([
         'name' => 'John Doe',
         'email' => 'john@example.com',
-        'password' => Hash::make('password'),
-        'password_confirmation' => Hash::make('password'),
+        'password' =>$password,
+        'password_confirmation' => $password,
     ]);
 
     // Attempt to log in with the created user's credentials
-    $response = $this->postJson('/api/login', [
+    $response = $this->postJson('/api/login',[
+        'name' => 'John Doe',
         'email' => 'john@example.com',
-        'password' => 'password',
-        'password_confirmation' => 'password',
+        'password' => "password",
+        'password_confirmation' => "password",
     ]);
 
     // Assert the response status and structure
