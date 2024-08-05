03240cfb237a5caf0790e35b6c7d18a827bb10df
unknown <dgloria.web@gmail.com>
2024-08-05 17:06:39 +0100

    added patreon related fields to users table

---
+++ database/migrations/2024_08_05_155547_add_patreon_columns_to_users_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            // 
+            $table->string('patreon_code');
+            $table->timestamp('patreon_expiry_date')->nullable();
+            $table->tinyInteger('patreon_daily_counter');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+        });
+    }
+};
