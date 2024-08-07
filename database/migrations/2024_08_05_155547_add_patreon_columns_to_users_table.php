877b67a55d42313f9bdcba96d0bc1305378e86ea
unknown <dgloria.web@gmail.com>
2024-08-07 22:27:40 +0100

    added default nulls to the new fields to be able to register users

--- database/migrations/2024_08_05_155547_add_patreon_columns_to_users_table.php
+++ database/migrations/2024_08_05_155547_add_patreon_columns_to_users_table.php
@@ -13,9 +13,9 @@ return new class extends Migration
     {
         Schema::table('users', function (Blueprint $table) {
             // 
-            $table->string('patreon_code');
+            $table->string('patreon_code')->nullable();
             $table->timestamp('patreon_expiry_date')->nullable();
-            $table->tinyInteger('patreon_daily_counter');
+            $table->tinyInteger('patreon_daily_counter')->nullable();
 
         });
     }
