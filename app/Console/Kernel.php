d1b345250c10aad2515d82b0cc4a2c594b94bdb5
unknown <dgloria.web@gmail.com>
2024-09-16 09:22:11 +0100

    scheduler updates

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,7 +13,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         // $schedule->command('inspire')->hourly();
-        $schedule->call('App\Http\Controllers\PatreonController@getPatrons')->everyHour();
+        // $schedule->call('App\Http\Controllers\PatreonController@getPatrons')->everyHour();
         $schedule->call('App\Http\Controllers\PatreonController@resetPatreonCounter')->dailyat('2:00');
 
     }
